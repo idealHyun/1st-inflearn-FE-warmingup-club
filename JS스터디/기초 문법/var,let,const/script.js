15abd5210e41df0d7602a69298ec293e6e984b05
SanghyunLee <lgw9736@naver.com>
2024-05-01 00:24:03 +0900

    스터디 폴더 생성

--- JS스터디/기초 문법/var,let,const/script.js
+++ JS스터디/기초 문법/var,let,const/script.js
@@ -5,21 +5,23 @@ var hello="hello2"; //var은 중복선언 가능
 console.log(hello); 
 
 hello="hello3"; //var은 재할당 가능
-console.log(hello); 
+console.log(hello); //hello3
 
 
 let hi="hi"; 
 console.log(hi);
 
-let hi="hi2"; //오류 : let은 중복선언 불가능
-console.log(hi);
+// /let hi="hi2"; //오류 : let은 중복선언 불가능
+console.log(hi); 
 
 hi="hi3"; //let은 재할당 가능
+console.log(hi); //hi3  
 
 const hey="hey";
 console.log(hey);
 
-const hey="hey2"; //오류 :const는 중복선언 불가능
+// const hey="hey2"; //오류 :const는 중복선언 불가능
 console.log(hey);
 
-hey="hey3"; //오류 : const는 재할당 불가능
+hey="hey3"; // const는 재할당 불가능
+console.log(hey); // hey
